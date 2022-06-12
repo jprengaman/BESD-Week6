bee96ad23f13c86a55869fcf6dee9e8bd6463333
John Prengaman <prengaman78@gmail.com>
2022-06-11 23:51:46 -0700

    Create Value.java

---
+++ Value.java
@@ -0,0 +1,19 @@
+package com.promineotech.wargame;
+
+public enum Value {
+  
+  Two,
+  Three,
+  Four,
+  Five,
+  Six,
+  Seven,
+  Eight,
+  Nine,
+  Ten,
+  Jack,
+  Queen,
+  King,
+  Ace
+
+}
